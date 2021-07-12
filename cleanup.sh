5f3b29a74270a5309903b6af5e205bee1d02e2e2
Mahin Ahmed <XenonTheInertG@outlook.com>
2021-07-12 16:51:45 +0600

    clenup.sh:linux mirror fixed

--- cleanup.sh
+++ cleanup.sh
@@ -1,19 +1,40 @@
-#!/system/bin/sh
-# AIK-mobile/cleanup: reset working directory
+#!/bin/bash
+# Rec-Boot Bot/cleanup: reset working directory
 # XenonTheInertG @ xda-developers
 
 case $1 in
-  --help) echo "usage: cleanup.sh"; return 1;
+  --help) echo "usage: cleanup.sh [--local] [--quiet]"; exit 1;
 esac;
 
-case $0 in
-  *.sh) aik="$0";;
-     *) aik="$(lsof -p $$ 2>/dev/null | grep -o '/.*cleanup.sh$')";;
+case $(uname -s) in
+  Darwin|Macintosh)
+    statarg="-f %Su";
+    readlink() { perl -MCwd -e 'print Cwd::abs_path shift' "$2"; }
+  ;;
+  *) statarg="-c %U";;
 esac;
+
+aik="${BASH_SOURCE:-$0}";
 aik="$(dirname "$(readlink -f "$aik")")";
+bin="$aik/bin";
 
-cd "$aik";
-rm -rf ramdisk split_img *new.* *original.*;
-echo "Working directory cleaned.";
-return 0;
+case $1 in
+  --local) shift;;
+  *) cd "$aik";;
+esac;
+
+chmod -R 755 "$bin" "$aik"/*.sh;
+chmod 644 "$bin/magic" "$bin/androidbootimg.magic" "$bin/boot_signer.jar" "$bin/avb/"* "$bin/chromeos/"*;
+
+if [ -d ramdisk ] && [ "$(stat $statarg ramdisk | head -n 1)" = "root" -o ! "$(find ramdisk 2>&1 | cpio -o >/dev/null 2>&1; echo $?)" -eq "0" ]; then
+  sudo=sudo;
+fi;
+
+$sudo rm -rf ramdisk split_img *new.* || exit 1;
+
+case $1 in
+  --quiet) ;;
+  *) echo "Working directory cleaned.";;
+esac;
+exit 0;
 
