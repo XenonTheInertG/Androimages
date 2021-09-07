46cc07d9c07a8328062fb0b74e1ba9ddd6449dac
Mahin Ahmed <XenonTheInertG@outlook.com>
2021-09-07 15:30:20 +0600

    v3.8 - add hdr_v3 boot/vendor_boot support, improve lz4 support, fixes

--- .script/cleanup.sh
+++ .script/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Rec-Boot Bot/cleanup: reset working directory
-# OsmOsis @ xda-developers
+# XenonTheInertG & OsmOsis @ xda-developers
 
 case $1 in
   --help) echo "usage: cleanup.sh [--local] [--quiet]"; exit 1;
